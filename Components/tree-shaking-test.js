ed453c03a080b7dec730fc8c7fd3063e8c55abdc
Eric Noguchi <eric_noguchi@hotmail.com>
2017-02-08 00:56:28 -0500

    some js doc testing

--- Components/tree-shaking-test.js
+++ Components/tree-shaking-test.js
@@ -1,2 +1,8 @@
+/**
+ * This is a function that adds to numbers
+ * @param {number} a number
+ * @param {number} b  anoterh number
+ * @return {number} result of add
+ */
 export let add = (a, b) => a + b;
 export let substract = (a, b) => a - b;
